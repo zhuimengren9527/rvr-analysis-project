f0c88360953d8ce388c4f071bd2dc290f099550e
zhuimengren9527 <277812274@qq.com>
2026-06-29 20:26:55 +0800

    refactor: split inspect_data into modular functions

--- src/inspect_data.py
+++ src/inspect_data.py
@@ -2,16 +2,12 @@ from datetime import datetime
 from pathlib import Path
 import pandas as pd
 
-def inspect_data(file_path):
 
-    # 创建文件对象
-    path = Path(file_path)
+# ===================================================
+# 提取文件信息（名称、大小、修改时间）
+# ===================================================
 
-    # 判断文件是否存在
-    if not path.exists():
-        print("文件不存在")
-
-        return
+def get_file_info(path):
     # 打印文件名称
     print(f"文件名称:{path.name}")
 
@@ -33,3 +29,56 @@ def inspect_data(file_path):
     modified_time = path.stat().st_mtime
     modified_time = datetime.fromtimestamp(modified_time)
     print(f"文件修改时间:{modified_time:%Y-%m-%d %H:%M:%S}")
+
+
+    # =============================================================
+    # 文件预览
+    # =============================================================
+
+def preview_file(path):
+
+    with open(path,'r',encoding='latin1') as f:
+
+        print(f"\n预览数据前10行:\n")
+        lines = []
+        for i in range(10):
+            line = f.readline()
+            lines.append(line)
+            print(repr(line))
+    return lines
+
+    # =============================================================
+    # 显示文件分隔符
+    # =============================================================
+def detect_separator(lines):
+
+    sample_text = "".join(lines)
+
+    if '\t' in sample_text:
+        print("\n检测到分隔符：Tab (\\t)")
+    elif ',' in sample_text:
+        print("\n检测到分隔符：,")
+    elif ';' in sample_text:
+        print("\n检测到分号：;")
+    elif ' ' in sample_text:
+        print("\n检测到空格：空格")
+    return
+
+def inspect_data(file_path):
+
+# ===========================================================
+# 创建文件对象并判断文件是否存在（主函数）
+# =========================================================== 
+
+    path = Path(file_path)
+
+    if not path.exists():
+        print("文件不存在")
+        return
+
+    # 调用各函数工具
+    get_file_info(path)
+    lines = preview_file(path)
+    detect_separator(lines)
+
+
